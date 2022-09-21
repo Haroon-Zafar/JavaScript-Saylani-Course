eb624ce2ff2643bf33e735d1d3926a61b873571b
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-21 18:12:09 +0500

    Data Types.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // This is my first js code 
 alert("Hello World");
 alert("Hello User");
+
+
+// THERE ARE 3 DATA TYPES
+
+//  STRING 
+
+//  "abcd!1231!^@%&$*!@#" ANY TYPE OF TEXT CAN BE WRITTEN IN STRING DATA TYPE
+
+// NUMBER 
+
+//  5 123 1.012 -2 -123.123
+
+
+//  BOOLEAN 
+
+// TRUE FALSE 
+
