859e8676e637d53fb0d44b66d7581f17ef3953fa
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-26 23:57:14 +0500

    Pyramid Pattern

--- index.js
+++ index.js
@@ -334,3 +334,15 @@ var months = ["January", "February", "April", 4];
 * * * * *
 
 */
+
+for ( var i = 1 ; i <= 5 ; i++ ){
+
+    var star = "";
+
+    for ( var j = 1 ; j <= i ; j ++ ){
+        star = star + "* ";
+
+    }
+    console.log(star);
+
+}
